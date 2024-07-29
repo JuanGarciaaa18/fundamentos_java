2e29a975c7b11d005260b931fe1d9ae26e703973
SENA <juanmanuelgarciaespinosa@gmail.com>
2024-07-29 07:36:39 -0500

    finish 29/07

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 
 public class Main {
+    private static Object x;
+
     public static void ejercicio1() {
         double x = (double) 5.89 + (double) 3.45;
         System.out.println(x);
@@ -46,5 +48,177 @@ public class Main {
 
     }
 
+    public static void ejercicio8(){
+        float numeroFloat = 3.14159f;
+        String resultado = String.valueOf(numeroFloat);
+
+        System.out.println("El numero float convertido a String es: " + resultado);
+
+    }
+
+
+    public static void ejercicio9(){
+        int resultado = (int) Math.round(3.7);
+
+        System.out.println("El resultado es: " + resultado);
+
+    }
+
+
+    public static void ejercicio10(){
+
+        byte b = 50;
+        short sh = (short) b;
+        System.out.println("El resultado es: " + sh);
+
+    }
+
+
+
+    public static void ejercicio11(){
+
+        int x = 10; x += 5;
+        System.out.println("El resultado es: " + x);
+
+    }
+
+
+    public static void ejercicio12(){
+        int y = 10;
+        int x = 3;
+        y *= (x + 5);
+        System.out.println("El valor de y es: " + y);
+    }
+
+
+    public static void ejercicio13(){
+        int a = 15;
+        a %= 4;
+        System.out.println("El valor de a es: " + a);
+    }
+
+    public static void ejercicio14(){
+        int x = 5;
+        int y = 2;
+        x += y;
+        System.out.println("El valor de x es: " + x);
+    }
+    public static void ejercicio15(){
+        int x=5;
+         x ^= 2;
+        System.out.println("El valor de x es: " + x);
+    }
+
+    public static void ejercicio16(){
+        int x = 5;
+        int y = ++x;
+        System.out.println("El valor de y es: " + y);
+    }
+
+    public static void ejercicio17(){
+        int a = 5;
+        System.out.println(a++);
+        System.out.println(a);
+    }
+
+    public static void ejercicio18(){
+        int count =4;
+        System.out.println(count--);
+    }
+
+    public static void ejercicio19(){
+        int x =4;
+        x = ++x;
+        int y =4;
+        y = y++;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public static void ejercicio20(){
+        int x = 3;
+        x = x++;
+        System.out.println(x);
+    }
+
+    public static void ejercicio21(){
+        int i = 5;
+        i += ++i + i++ + ++i;
+        System.out.println("El valor de i es:" +i);
+
+    }
+
+
+    public static void ejercicio22(){
+        double x = 9.1;
+        int z = (int)x;
+        System.out.println("El valor de x es:" + ++z);
+
+    }
+
+    public static void ejercicio23(){
+        double d = 5.7;
+        int i = (int)d;
+        i *= 2;
+        System.out.println(i--);
+
+    }
+
+    public static void ejercicio24(){
+        int x=10;
+        double z= (double)x;
+        z /= 2;
+
+        System.out.println("El valor de x es: " + z);
+
+    }
+
+    public static void ejercicio25(){
+        char c = 'X';
+        c += 32;
+        System.out.println("El resultado es: "+c);
+    }
+
+
+    public static void ejercicio26(){
+        long x=10;
+        short z= (short) x;
+        z *= 3;
+
+        System.out.println("El valor de x es: " + z);
+    }
+
+    public static void ejercicio27(){
+        int x = 10;
+        x += (x++) + (++x);
+        System.out.println("El valor de x es: " + x);
+    }
+
+    public static void ejercicio28(){
+        float x= 10;
+        byte z= (byte) x;
+        z -= 3;
+
+        System.out.println("El valor de x es: " + z);
+
+    }
+
+    public static void ejercicio29(){
+        int i = 257;
+        byte b = (byte)i;
+        System.out.println(b);
+
+        System.out.println("El valor de b es: " + b);
+
+    }
+
+    public static void ejercicio30(){
+        double i = 10.0;
+        int b = (int)i;
+        b += 5 +5;
+        System.out.println(++b);
+
+
+    }
 
 }
--- src/solve.java
+++ src/solve.java
@@ -32,21 +32,115 @@ public class solve {
             case 7:
                 Main.ejercicio7();
                 break;
+            case 8:
+                Main.ejercicio8();
+                break;
+            case 9:
+                Main.ejercicio9();
+                break;
+            case 10:
+                Main.ejercicio10();
+                break;
+            case 11:
+                Main.ejercicio11();
+                break;
+            case 12:
+                Main.ejercicio12();
+                break;
+            case 13:
+                Main.ejercicio13();
+                break;
+            case 14:
+                Main.ejercicio14();
+                break;
+            case 15:
+                Main.ejercicio15();
+                break;
+            case 16:
+                Main.ejercicio16();
+                break;
+            case 17:
+                Main.ejercicio17();
+                break;
+            case 18:
+                Main.ejercicio18();
+                break;
+            case 19:
+                Main.ejercicio19();
+                break;
+            case 20:
+                Main.ejercicio20();
+                break;
+            case 21:
+                Main.ejercicio21();
+                break;
+            case 22:
+                Main.ejercicio22();
+                break;
+            case 23:
+                Main.ejercicio23();
+                break;
+            case 24:
+                Main.ejercicio24();
+                break;
+            case 25:
+                Main.ejercicio25();
+                break;
+            case 26:
+                Main.ejercicio26();
+                break;
+            case 27:
+                Main.ejercicio27();
+                break;
+            case 28:
+                Main.ejercicio28();
+                break;
+            case 29:
+                Main.ejercicio29();
+                break;
+            case 30:
+                Main.ejercicio30();
+                break;
+
             default:
                 System.out.println("Opción inválida. Intente de nuevo.");
         }
-    } while (opcion != 5);
+    } while (opcion != 31);
 
         scanner.close();
 }
 
 public static void mostrarMenu() {
     System.out.println("----- MENÚ PRINCIPAL -----");
-    System.out.println("1. Sumar dos números");
-    System.out.println("2. Restar dos números");
-    System.out.println("3. Multiplicar dos números");
-    System.out.println("4. Dividir dos números");
-    System.out.println("5. Salir del programa");
-    System.out.println("--------------------------");
+    System.out.println("Ejercicio 1");
+    System.out.println("Ejercicio 2");
+    System.out.println("Ejercicio 3");
+    System.out.println("Ejercicio 4");
+    System.out.println("Ejercicio 5");
+    System.out.println("Ejercicio 6");
+    System.out.println("Ejercicio 7");
+    System.out.println("Ejercicio 8");
+    System.out.println("Ejercicio 9");
+    System.out.println("Ejercicio 10");
+    System.out.println("Ejercicio 11");
+    System.out.println("Ejercicio 12");
+    System.out.println("Ejercicio 13");
+    System.out.println("Ejercicio 14");
+    System.out.println("Ejercicio 15");
+    System.out.println("Ejercicio 16");
+    System.out.println("Ejercicio 17");
+    System.out.println("Ejercicio 18");
+    System.out.println("Ejercicio 19");
+    System.out.println("Ejercicio 20");
+    System.out.println("Ejercicio 21");
+    System.out.println("Ejercicio 22");
+    System.out.println("Ejercicio 23");
+    System.out.println("Ejercicio 24");
+    System.out.println("Ejercicio 25");
+    System.out.println("Ejercicio 26");
+    System.out.println("Ejercicio 27");
+    System.out.println("Ejercicio 28");
+    System.out.println("Ejercicio 29");
+    System.out.println("Ejercicio 30");
 }
 }
